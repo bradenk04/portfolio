68814a0bd239d9f79eb121c91a302e30a31aaf91
Braden <bradenk04@proton.me>
2026-05-02 15:23:30 -0700

    feat: Updated SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-	},
-
-  integrations: [solidJs({
-      include: ['**/solid/*', '**/node_modules/@suid/material/**'],
-  })],
+    site: 'https://bradenkennedy.com',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [
+        sitemap(),
+        solidJs({
+            include: ['**/solid/*', '**/node_modules/@suid/material/**'],
+        }),
+    ],
 });
